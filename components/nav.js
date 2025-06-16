d2950d1a24985abd1c26eddd9ffd04db7cd6dfe8
Ray Chou <bboy10121988@gmail.com>
2025-06-16 11:20:17 +0800

    Update 'Contact Us' links to direct to LINE

--- components/nav.js
+++ components/nav.js
@@ -31,7 +31,7 @@ class Navigation extends HTMLElement {
                         <li><a href="rental.html">海放你基地</a></li>
                         <li><a href="plan.html">海放計畫</a></li>
                         <li><a href="kids-camp.html">海放兒童營隊</a></li>
-                        <li><a href="#contact" class="cta-button">聯繫我們</a></li>
+                        <li><a href="https://line.me/R/ti/p/@havefunnybase" target="_blank" class="cta-button">聯繫我們</a></li>
                     </ul>
                 </nav>
             </div>
@@ -47,7 +47,7 @@ class Navigation extends HTMLElement {
                     <li><a href="rental.html">海放你基地</a></li>
                     <li><a href="plan.html">海放計畫</a></li>
                     <li><a href="kids-camp.html">海放兒童營隊</a></li>
-                    <li><a href="#contact" class="cta-button">聯繫我們</a></li>
+                    <li><a href="https://line.me/R/ti/p/@havefunnybase" target="_blank" class="cta-button">聯繫我們</a></li>
                 </ul>
             </div>
 
@@ -358,24 +358,6 @@ class Navigation extends HTMLElement {
         });
 
         // 添加手機版側邊欄的事件處理
-        // 添加事件監聽器
-        const contactLinks = this.querySelectorAll('a[href="#contact"]');
-        contactLinks.forEach(link => {
-            link.addEventListener('click', (e) => {
-                e.preventDefault();
-                const contactSection = document.querySelector('#contact');
-                if (contactSection) {
-                    contactSection.scrollIntoView({ behavior: 'smooth' });
-                    
-                    // 如果在手機版，點擊後關閉側邊選單
-                    const mobileSidebar = document.querySelector('.mobile-sidebar');
-                    if (mobileSidebar && mobileSidebar.classList.contains('active')) {
-                        mobileSidebar.classList.remove('active');
-                    }
-                }
-            });
-        });
-
         // 添加事件監聽器
         const hamburgerMenu = this.querySelector('.hamburger-menu');
         const mobileSidebar = this.querySelector('.mobile-sidebar');
